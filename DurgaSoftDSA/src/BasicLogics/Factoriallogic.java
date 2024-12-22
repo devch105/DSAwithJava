f7441594388af1b367ac80e83bf02ec118ab5ef4
devch105 <devdhama23@gmail.com>
2024-12-22 15:40:22 +0530

    DSA java

--- DurgaSoftDSA/src/BasicLogics/Factoriallogic.java
+++ DurgaSoftDSA/src/BasicLogics/Factoriallogic.java
@@ -8,7 +8,7 @@ public class Factoriallogic {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        System.out.println(" Enter Number : ");
+
         int n =sc.nextInt();
 
         //Logic 1: Loop
