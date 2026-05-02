5d299e7d0b15251226f2f059b7c9e73fdff6df93
devch105 <devdhama23@gmail.com>
2026-05-02 23:51:13 +0530

    Add .gitignore file

---
+++ AlphaDSA/src/AlgorithimPatterns/TwoPointers_SlidingWindow/MajorityElement_P169.java
@@ -0,0 +1,4 @@
+package AlgorithimPatterns.TwoPointers_SlidingWindow;
+
+public class MajorityElement_P169 {
+}
---
+++ AlphaDSA/src/AlgorithimPatterns/TwoPointers_SlidingWindow/MaxmimumproductSubarray_P152.java
@@ -0,0 +1,4 @@
+package AlgorithimPatterns.TwoPointers_SlidingWindow;
+
+public class MaxmimumproductSubarray_P152 {
+}
