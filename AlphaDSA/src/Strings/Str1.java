80f667136f69547f4e373a774e5636793cf414a1
devch105 <devdhama23@gmail.com>
2026-08-19 17:46:35 +0530

    Stream Function use Longest String in a List

--- AlphaDSA/src/Strings/Str1.java
+++ AlphaDSA/src/Strings/Str1.java
@@ -1,16 +1,19 @@
 package Strings;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class Str1 {
     public static void main(String[] args) {
-//         String s=new String("abcc");
-//         String b=  s.concat("def");
-//        System.out.println(s);
-//        System.out.println(b);
+        List<String> words = List.of("Java", "Comparator", "Kubernetes", "Docker", "Microservices");
 
-        StringBuffer sb=new StringBuffer("Dev");
-        sb.append("Dhama");
-        System.out.println(sb);
+        String longest = words.stream().max(Comparator.comparingInt(String::length)).orElse("");
 
-        // 
+        String longest2 = words.stream().sorted(Comparator.comparingInt(String::length).reversed()).findFirst()
+                .orElse("");
+
+        System.out.println("Longest String is : " + longest);
+        System.out.println("Longest String is : " + longest2);
+
     }
 }
