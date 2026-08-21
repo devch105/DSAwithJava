8c4b5aea1a2e4adc009c5f5ccf849fb353338703
devch105 <devdhama23@gmail.com>
2026-08-21 14:41:06 +0530

    Coins Change Memo + Tabulation

--- AlphaDSA/src/DP/P_322.java
+++ AlphaDSA/src/DP/P_322.java
@@ -1,12 +1,46 @@
 package DP;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class P_322 {
     public static void main(String[] args) {
         int[] coins = { 2, 5, 10, 1 };
         int amount = 27;
         System.out.println("Answer : " + coinChange(coins, amount));
+
+        Map<String, Integer> map = new HashMap<>();
+        System.out.println("Answer Just checking : " + coinChangeMemo(coins, map, amount, 0));
+        System.out.println("Map : "+map);
+    }
+
+    public static int coinChangeMemo(int coins[], Map<String, Integer> map, int amount, int i) {
+        if (amount == 0) {
+            return 0;
+        }
+        if (i >= coins.length) {
+
+            return Integer.MAX_VALUE - 1;
+        }
+        String key = i + "_" + amount;
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+
+        if (coins[i] <= amount) {
+
+            int ans1 = 1 + coinChangeMemo(coins, map, amount - coins[i], i);
+            int ans2 = coinChangeMemo(coins, map, amount, i + 1);
+
+            int result = Math.min(ans1, ans2);
+            map.put(key, result);
+            return map.get(key);
+        } else {
+            int exclude = coinChangeMemo(coins, map, amount, i + 1);
+            map.put(key, exclude);
+            return map.get(key);
+        }
     }
 
     public static int coinChange(int[] coins, int amount) {
