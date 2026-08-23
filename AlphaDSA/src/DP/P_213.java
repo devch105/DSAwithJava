91b6b5f43a68a9ea63d9e1e85cc094be8ae8570f
devch105 <devdhama23@gmail.com>
2026-08-23 14:45:30 +0530

    update House Robber Method

--- AlphaDSA/src/DP/P_213.java
+++ AlphaDSA/src/DP/P_213.java
@@ -4,35 +4,34 @@ import java.util.Arrays;
 
 public class P_213 {
     public static void main(String[] args) {
-
-        int arr[] = { 1, 2, 3, 1 };
-        int dp[] = new int[arr.length];
-
-        Arrays.fill(dp, -1);
-        int first = solve(0, arr.length-2, arr, dp);
-        Arrays.fill(dp, -1);
-        int second = solve(0, arr.length-1, arr, dp);
-
-        int Result = Math.max(first, second);
-
-        System.out.println("Result : "+Result);
-
-      
-
+        int[] house = { 1 };
+        if (house.length == 1) {
+            System.out.println("Answer : " + house[0]);
+        } else {
+            System.out.println("Answer : " + rob(house));
+        }
     }
 
-    public static int solve(int i, int x, int arr[], int dp[]) {
-        if (i >= x)
-            return 0;
+    public static int rob(int[] arr) {
+        int dp[] = new int[arr.length];
+        Arrays.fill(dp, -1);
+        int first = solve(0, arr.length - 2, arr, dp);
+        Arrays.fill(dp, -1);
+        int second = solve(1, arr.length - 1, arr, dp);
+        return Math.max(first, second);
+    }
 
+    public static int solve(int i, int x, int nums[], int dp[]) {
+        if (i > x) {
+            return 0;
+        }
         if (dp[i] != -1) {
             return dp[i];
         }
+        int ans1 = nums[i] + solve(i + 2, x, nums, dp);
+        int ans2 = solve(i + 1, x, nums, dp);
 
-        int rob = arr[i] + solve(i + 2, x, arr, dp);
-        int leave = solve(i + 1, x, arr, dp);
-
-        dp[i] = Math.max(leave, rob);
+        dp[i] = Math.max(ans1, ans2);
         return dp[i];
     }
 }
