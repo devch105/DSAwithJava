0739f05d517d67a3dbf202d87df2079d2b43520c
devch105 <devdhama23@gmail.com>
2026-09-05 20:09:06 +0530

    P:560 : SubArray Sum equals K In Depth 3 Approaches

--- AlphaDSA/src/algorithimPatterns/TwoPointers/SubarraySumEqualsK_P560.java
+++ AlphaDSA/src/algorithimPatterns/TwoPointers/SubarraySumEqualsK_P560.java
@@ -7,8 +7,53 @@ public class SubarraySumEqualsK_P560 {
         int arr [] ={1,2,3,4,5,1,2,3};
         int k = 5;
         System.out.println("Answer : "+subarraySum(arr,k));
+        System.out.println("Brute Force : "+brute(arr, k));
 
     }
+    // Brute Force
+    public static int brute(int nums[], int k){
+        int count=0;
+        for(int i=0; i<nums.length; i++){
+            int sum =0;
+            for(int j=i; j<nums.length; j++){
+                sum+=nums[j];
+            if(sum==k)count++; 
+            }
+        }
+        return count;
+    }
+
+    // PrefixSum + HashMap
+
+      public int sumEqualsK(int nums[], int k){
+        int count=0;
+
+        // calculate prefix
+        int prefix [] = new int[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1; i<prefix.length; i++){
+            prefix[i]=prefix[i-1]+nums[i];
+        }
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0; i<prefix.length; i++){
+             
+            if(prefix[i]==k){
+                count++;
+            }
+
+            int val = prefix[i]-k;
+            if(map.containsKey(val)){
+                count +=map.get(val);
+            }
+            
+                map.put(prefix[i],map.getOrDefault(prefix[i],0)+1);
+            }
+        return count;
+    }
+
+    // Prefix Variable  + HashMap
+
     public  static int subarraySum(int nums[],int k  ) {
         int count = 0;
         int sum = 0;
@@ -26,4 +71,6 @@ public class SubarraySumEqualsK_P560 {
         }
         return count;
     }
+
+   
 }
