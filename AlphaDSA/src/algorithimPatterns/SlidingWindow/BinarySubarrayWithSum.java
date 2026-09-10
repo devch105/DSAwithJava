afe510e17909941a3e8e56805ca51092ed7cfa9a
devch105 <devdhama23@gmail.com>
2026-09-10 20:15:51 +0530

    nil

--- AlphaDSA/src/algorithimPatterns/SlidingWindow/BinarySubarrayWithSum.java
+++ AlphaDSA/src/algorithimPatterns/SlidingWindow/BinarySubarrayWithSum.java
@@ -1,15 +1,6 @@
 package algorithimPatterns.SlidingWindow;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 import java.util.HashMap;
-
-/**
- *
- * @author predator
- */
 public class BinarySubarrayWithSum {
 
     public static void main(String[] args) {
